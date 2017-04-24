22c9dab09297ed968587ad66807e1ed3461d4090
Darren Sun <sunzhe_c2702@163.com>
2017-04-24 21:39:31 +0800

    LeetCode: Problem5: add the problem detail

--- LeetCode/Longest_Palindromic_Substring.c
+++ LeetCode/Longest_Palindromic_Substring.c
@@ -1,3 +1,24 @@
+/*
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+Example:
+
+Input: "cbbd"
+
+Output: "bb"
+Subscribe to see which companies asked this question.
+
+Show Tags
+Show Similar Problems
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
